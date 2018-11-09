4fcf680f6d0f5fa12d4dccc32c159b8c607ae0af
Haemin Yoo <haemin.yoo@paidy.com>
2018-11-09 17:19:03 +0900

    Trying to figure out stuff but not yet

--- build.sbt
+++ build.sbt
@@ -4,4 +4,9 @@ scalaVersion in ThisBuild := "2.11.12"
 // Set to false or remove if you want to show stubs as linking errors
 nativeLinkStubs := true
 
+cancelable in Global := true
+
+libraryDependencies ++= {
+  Seq()
+}
 enablePlugins(ScalaNativePlugin)
--- src/main/scala/tcp/Main.scala
+++ src/main/scala/tcp/Main.scala
@@ -1,12 +1,27 @@
 package tcp
 
+import scala.scalanative.native._
+import scala.scalanative.posix.sys.socket.{ socket, AF_INET, SOCK_STREAM }
+import scala.scalanative.posix.netinet.in.{ sockaddr_in, INADDR_ANY }
+import scala.scalanative.posix.arpa.inet.{ htons, htonl }
+
 sealed trait Status
-case object Ok extends Status
-case class Error(msg: String) extends Status
+object Status {
+  case object Ok extends Status
+  case class Error(msg: String) extends Status
+}
 
-case class Server(listenFd: Int) {
 
-  def listen(): Status = ???
+class Server(val listenFd: CInt) extends AnyVal {
+
+  def listen(): Status = {
+    val result = socket(AF_INET, SOCK_STREAM, 0)
+
+    if (result == -1)
+      Status.Error("Failed to create socket endpoint")
+    else
+      Status.Ok
+  }
 
   def accept(): Status = ???
 
@@ -14,25 +29,40 @@ case class Server(listenFd: Int) {
 
 object Main {
 
-  val Port = 0
+  val Port = 1234
+
+  val serverAddr = stackalloc[sockaddr_in]
+  !serverAddr._1 = AF_INET.toUShort //sin_family
+  !serverAddr._2 = htons(Port.toUShort) //sin_addr
+  !serverAddr._3 = new sockaddr_in(htonl(INADDR_ANY)) //sin_port
 
   def main(args: Array[String]): Unit = {
-    val server = Server(0)
+    val server = new Server(0)
 
     server.listen() match {
-      case Error(msg) =>
+      case Status.Error(msg) =>
         System.err.println(s"Failed to listen on address 0.0.0.0:$Port\n$msg\n")
 
-      case Ok =>
+      case Status.Ok =>
         while (true) {
           server.accept() match {
-            case Error(msg) =>
+            case Status.Error(msg) =>
               System.err.println(s"Failed accepting connection\n")
               System.exit(1)
-            case Ok =>
+            case Status.Ok =>
           }
         }
     }
 
   }
+}
+
+
+object Foo {
+
+  type Server = CStruct1[Int]
+  val server = stackalloc[Server]
+
+  !server
+
 }
